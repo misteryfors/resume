0a31cc1e6f10a499c07f76345de8c1c84e550d46
misteryfors <47424667+misteryfors@users.noreply.github.com>
2024-12-04 14:56:55 +0300

    Add files via upload

--- src/content/main/project-list.js
+++ src/content/main/project-list.js
@@ -8,7 +8,8 @@ const projectsData1rus = [
     {href:"#prod", img:"Desktop (1600 px).png", badges:[1,8], num:"project 1", title:"Weather-App", description:"Этот проект представляет собой простой сайт с использованием только базовых инструментов HTML, JS и CSS. Выполнено по макету из акселератора опыта Preax. А также добавленно получение данных с WeatheAPI.", date:"2024.10.21", model:"https://www.figma.com/design/60Iap3oS9KpQ6XPuBK8CZ2/Weather-App.-1-sprint.-4-task?node-id=4056-3513&node-type=frame&t=2mTZerPDj5WoYQDM-0",site:"weather-app.html"},
     {href:"#prod", img:"to-do-list.png", badges:[1,4,8], num:"project 2", title:"To-Do-list", description:"Простой to-do лист без адаптива на мобильные устройства, с реализованным и улучшенным drag-and-drop добавлением и редактированием карточек с подлюченным localstorage", date:"2024.10.28", model:"https://www.figma.com/design/0InXcSFs6vm5c01OqQEnNs/Trello-board---auto-layout-cards-(Community)?node-id=5-1008&node-type=frame&t=PELKZ6f8NCrIyM7I-0", site:"to-do-list.html"},
     {href:"#prod", img:"crypto-landing.png", badges:[1,4,8], num:"project 3", title:"Crypto-landing", description:"Основная страница приложения для контроля своего криптовалютного портфеля, с графиками портфолио и подключёнными api", date:"2024.11.07", model:"https://www.figma.com/design/MXBe6O0vYuieKnzRjMQPQI/Figma-UI-kit---Cryptocurrency-Dashboard-V2-(Community)-(Community)?node-id=301-73213&node-type=canvas&t=UKDT1J44BGb7q1LM-0", site:"crypto.html"},
-    ];
+    {href:"#prod", img:"blog-website.png", badges:[1,4,5,8], num:"project 4", title:"Blog-website", description:"Основная страница выполненная на react, с помощью typescript на сборщике vite", date:"2024.12.04", model:"https://www.figma.com/design/Z2tbhTbu3O3EkYPVVnFJTO/AI-Blog-Website-UI-Template---Dark-Theme-%7C-Produce-UI-(Community)?node-id=324-8&node-type=frame&t=yDxX7YETz3dSSpUs-0", site:"blog-website.html"},
+];
 
 function renderBadges(badgeIds) {
     return badgeIds.map(badgeId => {
